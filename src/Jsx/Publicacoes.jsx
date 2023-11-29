b1a250ebcd9a89b8126d0271d4d217c2e431453d
ldbraz <ldbraz@latam.stefanini.com>
2023-11-29 16:08:41 -0300

    componentização do item de comentário

--- src/Jsx/Publicacoes.jsx
+++ src/Jsx/Publicacoes.jsx
@@ -7,7 +7,7 @@ import Edificio from "../html-css-template/imagens/image-edficio=devhub.svg";
 import arrowLeft from '../html-css-template/imagens/arrow-left (2).svg';
 import '../html-css-template/css/publicacoes.css'
 import { useState, useEffect } from 'react';
-
+import Comentario from "../components/Comentario";
 
 function Publicacoes() {
 
@@ -98,55 +98,7 @@ function Publicacoes() {
 
                 <div className="feed-body">
 
-                    <div className="post-item">
-                        <div className="post-user-infos">
-                            <div className="imagem-feed">
-                                <img src={fotoPerfil} alt="" width={"80px"} />
-                            </div>
-                            <div className="feed-name-function">
-                                <div className="feed-name">João Felipe</div>
-                                <div className="feed-function"> Analista de Projetos</div>
-                            </div>
-                        </div>
-                        <div className="text-feed">
-
-                            A EZLIV é uma empresa de Tecnologia da Informação (TI) dedicada à gestão de condomínios. Com uma abordagem centrada no cliente, nossa plataforma personalizada oferece soluções para simplificar a administração, melhorar a comunicação e otimizar a manutenção. Nosso compromisso com a qualidade e segurança é primordial, garantindo a proteção de dados e a integridade das informações.
-                            Nossa plataforma é flexível, adaptando-se às necessidades específicas de cada condomínio. Oferecemos uma experiência sem atritos para síndicos, condôminos e fornecedores, permitindo a colaboração em tempo real. Estamos empenhados em melhorar a qualidade de vida nos condomínios, promovendo um ambiente seguro e harmonioso.
-                        </div>
-                    </div>
-
-                    <div className="post-item">
-                        <div className="post-user-infos">
-                            <div className="imagem-feed">
-                                <img src={fotoPerfil} alt="" width={"80px"} />
-                            </div>
-                            <div className="feed-name-function">
-                                <div className="feed-name">João Felipe</div>
-                                <div className="feed-function"> Analista de Projetos</div>
-                            </div>
-                        </div>
-                        <div className="text-feed">
-
-                            A EZLIV é uma empresa de Tecnologia da Informação (TI) dedicada à gestão de condomínios. Com uma abordagem centrada no cliente, nossa plataforma personalizada oferece soluções para simplificar a administração, melhorar a comunicação e otimizar a manutenção. Nosso compromisso com a qualidade e segurança é primordial, garantindo a proteção de dados e a integridade das informações.
-                            Nossa plataforma é flexível, adaptando-se às necessidades específicas de cada condomínio. Oferecemos uma experiência sem atritos para síndicos, condôminos e fornecedores, permitindo a colaboração em tempo real. Estamos empenhados em melhorar a qualidade de vida nos condomínios, promovendo um ambiente seguro e harmonioso.
-                        </div>
-                    </div>
-                    <div className="post-item">
-                        <div className="post-user-infos">
-                            <div className="imagem-feed">
-                                <img src={fotoPerfil} alt="" width={"80px"} />
-                            </div>
-                            <div className="feed-name-function">
-                                <div className="feed-name">João Felipe</div>
-                                <div className="feed-function"> Analista de Projetos</div>
-                            </div>
-                        </div>
-                        <div className="text-feed">
-
-                            A EZLIV é uma empresa de Tecnologia da Informação (TI) dedicada à gestão de condomínios. Com uma abordagem centrada no cliente, nossa plataforma personalizada oferece soluções para simplificar a administração, melhorar a comunicação e otimizar a manutenção. Nosso compromisso com a qualidade e segurança é primordial, garantindo a proteção de dados e a integridade das informações.
-                            Nossa plataforma é flexível, adaptando-se às necessidades específicas de cada condomínio. Oferecemos uma experiência sem atritos para síndicos, condôminos e fornecedores, permitindo a colaboração em tempo real. Estamos empenhados em melhorar a qualidade de vida nos condomínios, promovendo um ambiente seguro e harmonioso.
-                        </div>
-                    </div>
+                    <Comentario />
 
 
                 </div>
