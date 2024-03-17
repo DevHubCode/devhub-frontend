3743b9b4c5780c867ad2ff018322b1598a63ddc3
RaulFOliveira <rauloliveira163@gmail.com>
2024-03-17 20:20:02 -0300

    fix: Arrumando o proxy da ec2

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,6 @@ const { createProxyMiddleware } = require("http-proxy-middleware")
 
 module.exports = function(app) {
     app.use(
-        '/api',
         createProxyMiddleware({
             target: "http://10.0.0.206:8080",
             changeOrigin: true
