8570474402382271b71887a50e5f5a7bc22320bf
RaulFOliveira <rauloliveira163@gmail.com>
2024-03-17 21:31:06 -0300

    fix: Arrumando o URI das requisições

--- src/api.js
+++ src/api.js
@@ -4,5 +4,17 @@ const api = axios.create({
   baseURL: '/api'
 });
 
+api.interceptors.request.use(
+  function (config) {
+    config.url = `http://10.0.0.206:8080`;
+
+    config.headers['Access-Control-Allow-Origin'] = '*';
+
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
