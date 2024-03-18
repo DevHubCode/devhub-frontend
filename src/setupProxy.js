68ccb8edc0530387905fbb775fa23f058a783b42
RaulFOliveira <rauloliveira163@gmail.com>
2024-03-17 21:01:22 -0300

    fix: Arrumando o URI das requisições

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,10 +2,12 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function(app) {
     app.use(
-        '',
-        createProxyMiddleware({
+        createProxyMiddleware('/api', {
             target: "http://10.0.0.206:8080",
             changeOrigin: true,
+            pathRewrite: {
+                '^/api': ''
+            }
         })
     );
 };
